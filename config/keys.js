aef9c7f6367b09433e4d612ce7c072a4299c937a
github username <github email address>
2018-12-06 12:05:50 -0500

    auth setup

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://chris:chris123@ds231133.mlab.com:31133/linkedin"
+  mongoURI: "mongodb://chris:chris123@ds231133.mlab.com:31133/linkedin",
+  secretOrKey: "secret"
 };
